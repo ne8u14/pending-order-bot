df117a752a55fd3d0ccf0ec7e41bc9fac43c2d32
vanilla <101264439+ne8u14@users.noreply.github.com>
2022-07-04 23:36:21 +0800

    update

--- src/dfxJson.ts
+++ src/dfxJson.ts
@@ -3,7 +3,7 @@ import { Principal } from '@dfinity/principal';
 import { CanisterDto, CanisterJson } from './pendingOrder.dto';
 
 export const get_canister_json = (): CanisterJson => {
-  const file_content = fs.readFileSync('./assets/canister.json', 'utf8');
+  const file_content = fs.readFileSync('./src/assets/canister.json', 'utf8');
   return JSON.parse(file_content);
 };
 
@@ -14,5 +14,6 @@ export const get_host = (): string => {
 
 export const get_canister_id = (name: string): Principal => {
   const canister_json = get_canister_json();
+  console.log(canister_json.canisters[name]);
   return Principal.fromText(canister_json.canisters[name]);
 };
